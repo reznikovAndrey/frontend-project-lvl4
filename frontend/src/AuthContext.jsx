68f5e389e21f7852556da5df0f58375a0adc373b
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-07-17 00:44:49 +0500

    add `AuthContext`
    Auth context works with local storage.
    Function `login` set key `user` with provided data as value.
    Function `logout` remove this key.
    Initial state `loggedIn` depends on existance `user` key in local storage.

---
+++ frontend/src/AuthContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+
+const AuthContext = createContext({});
+
+const AuthProvider = ({ children }) => {
+  const isLoggedIn = localStorage.getItem('user');
+  const [loggedIn, setLoggedIn] = useState(!!isLoggedIn);
+
+  const login = (data) => {
+    localStorage.setItem('user', JSON.stringify(data));
+    setLoggedIn(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    setLoggedIn(false);
+  };
+
+  const memoizedValue = useMemo(() => ({ loggedIn, login, logout }), [loggedIn]);
+
+  return <AuthContext.Provider value={memoizedValue}>{children}</AuthContext.Provider>;
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export { AuthProvider, AuthContext };
