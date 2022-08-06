671f04b4fdbc87f60c6139e85d98093958b3b3b7
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-08-06 21:06:54 +0500

    add validation during channel creation

--- frontend/src/components/forms/AddChannelForm.jsx
+++ frontend/src/components/forms/AddChannelForm.jsx
@@ -2,16 +2,31 @@ import { useFormik } from 'formik';
 import { useEffect, useRef } from 'react';
 import { Form, InputGroup } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import * as Yup from 'yup';
 
 import { useSocket } from '../../hooks';
 
 const AddChannelForm = ({ closeModal }) => {
   const socket = useSocket();
+  const { channels } = useSelector((state) => state.chats);
+
+  const { t } = useTranslation();
 
   const formik = useFormik({
     initialValues: {
       channelName: '',
     },
+    validationSchema: Yup.object({
+      channelName: Yup.string()
+        .trim()
+        .notOneOf(
+          channels.map(({ name }) => name),
+          ({ value }) => t('forms.addChannel.errors.duplicate', { channelName: value }),
+        )
+        .required(t('forms.addChannel.errors.required')),
+    }),
+    validateOnChange: false,
     onSubmit: async ({ channelName }) => {
       socket.newChannel({ name: channelName }, ({ status }) => {
         if (status === 'ok') {
@@ -26,7 +41,6 @@ const AddChannelForm = ({ closeModal }) => {
   const input = useRef(null);
   useEffect(() => input.current.focus(), []);
 
-  const { t } = useTranslation();
   return (
     <Form onSubmit={formik.handleSubmit} id="addChannelForm">
       <InputGroup>
@@ -36,7 +50,9 @@ const AddChannelForm = ({ closeModal }) => {
           value={formik.values.channelName}
           aria-label={t('forms.addChannel.fields.channelName.label')}
           ref={input}
+          isInvalid={formik.errors.channelName}
         />
+        <Form.Control.Feedback type="invalid">{formik.errors.channelName}</Form.Control.Feedback>
       </InputGroup>
     </Form>
   );
