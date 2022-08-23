e94131fefc3ffa1fb82882e1b4797e4707c197af
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-08-23 21:23:01 +0500

    fix: add limit for active channels quantity
    - update linter rules and translations
    - add contstant with max channels quantity
    - refactor `ChannelModal`: add functions `renderModalBody` and `renderModalFooter` for correct rendering

--- frontend/src/components/ChannelModal.jsx
+++ frontend/src/components/ChannelModal.jsx
@@ -7,10 +7,14 @@ import { ChannelForm } from './forms';
 
 import { useSocket } from '../hooks';
 import { actions } from '../slices/modalsSlice';
+import { MAX_CHANNELS_QUANTITY } from '../utils/constants';
 
 const ChannelModal = () => {
   const dispatch = useDispatch();
-  const { isShown, modalAction, channelId } = useSelector(({ modals }) => modals);
+  const {
+    modals: { isShown, modalAction, channelId },
+    chats: { channels },
+  } = useSelector((state) => state);
 
   const [disabled, setDisabled] = useState(false);
 
@@ -33,6 +37,52 @@ const ChannelModal = () => {
   };
 
   const { t } = useTranslation();
+  const channelsLimitWasReached = channels.length >= MAX_CHANNELS_QUANTITY;
+
+  const renderModalBody = () => {
+    switch (modalAction) {
+      case 'add':
+        return channelsLimitWasReached ? (
+          <p>{t(`modals.channel.${modalAction}.body`)}</p>
+        ) : (
+          <ChannelForm closeModal={closeModal} />
+        );
+      case 'remove':
+        return <p>{t(`modals.channel.${modalAction}.body`)}</p>;
+      case 'rename':
+        return <ChannelForm closeModal={closeModal} />;
+      default:
+        return null;
+    }
+  };
+
+  const renderModalFooter = () => {
+    switch (modalAction) {
+      case 'add':
+        return (
+          channelsLimitWasReached && (
+            <Modal.Footer>
+              <Button variant="secondary" onClick={closeModal}>
+                {t(`modals.channel.${modalAction}.cancelButtonText`)}
+              </Button>
+            </Modal.Footer>
+          )
+        );
+      case 'remove':
+        return (
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeModal}>
+              {t(`modals.channel.${modalAction}.cancelButtonText`)}
+            </Button>
+            <Button variant="danger" onClick={removeChannel} disabled={disabled}>
+              {t(`modals.channel.${modalAction}.confirmButtonText`)}
+            </Button>
+          </Modal.Footer>
+        );
+      default:
+        return null;
+    }
+  };
 
   if (!modalAction) {
     return null;
@@ -43,23 +93,8 @@ const ChannelModal = () => {
       <Modal.Header closeButton>
         <Modal.Title>{t(`modals.channel.${modalAction}.title`)}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        {modalAction === 'remove' ? (
-          <p>{t(`modals.channel.${modalAction}.body`)}</p>
-        ) : (
-          <ChannelForm closeModal={closeModal} />
-        )}
-      </Modal.Body>
-      {modalAction === 'remove' && (
-        <Modal.Footer>
-          <Button variant="secondary" onClick={closeModal}>
-            {t(`modals.channel.${modalAction}.cancelButtonText`)}
-          </Button>
-          <Button variant="danger" onClick={removeChannel} disabled={disabled}>
-            {t(`modals.channel.${modalAction}.confirmButtonText`)}
-          </Button>
-        </Modal.Footer>
-      )}
+      <Modal.Body>{renderModalBody()}</Modal.Body>
+      {renderModalFooter()}
     </Modal>
   );
 };
---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,1 @@
+export const MAX_CHANNELS_QUANTITY = 12;
