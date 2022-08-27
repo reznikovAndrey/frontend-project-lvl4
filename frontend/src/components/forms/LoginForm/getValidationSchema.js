ff7a1e0d8df37f504ec876bfc1d9f905efedd099
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-08-27 16:48:41 +0500

    fix: remove username validation during login

--- frontend/src/components/forms/LoginForm/getValidationSchema.js
+++ frontend/src/components/forms/LoginForm/getValidationSchema.js
@@ -1,9 +1,7 @@
 import * as Yup from 'yup';
 
-const regexp = /^\w+$/;
-
 export default () =>
   Yup.object({
-    username: Yup.string().matches(regexp, 'forms.login.fields.username.error').required('forms.login.errors.required'),
+    username: Yup.string().trim().required('forms.login.errors.required'),
     password: Yup.string().required('forms.login.errors.required'),
   });
