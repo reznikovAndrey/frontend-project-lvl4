1fb21b9f5532dae4c6eba24db4337d3342388d27
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-09-04 14:07:19 +0500

    configure `rollbar`
    - install  and configure `@rollbar/react`
    - add `.env` with secets and update `.gitignore`

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,3 +1,4 @@
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import i18next from 'i18next';
 import filter from 'leo-profanity';
 import { initReactI18next, I18nextProvider } from 'react-i18next';
@@ -6,12 +7,20 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { AuthProvider, SocketProvider } from './contexts';
-
 import { en, ru } from './locales';
 import store from './slices';
 
 export default async (socket) => {
   const i18n = i18next.createInstance();
+
+  const rollbarConfig = {
+    accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+      enviroment: process.env.NODE_ENV,
+    },
+  };
 
   filter.add(filter.getDictionary('ru'));
   filter.add(filter.getDictionary('en'));
@@ -24,16 +33,20 @@ export default async (socket) => {
   });
 
   return (
-    <I18nextProvider i18n={i18n}>
-      <BrowserRouter>
-        <Provider store={store}>
-          <AuthProvider>
-            <SocketProvider socket={socket}>
-              <App />
-            </SocketProvider>
-          </AuthProvider>
-        </Provider>
-      </BrowserRouter>
-    </I18nextProvider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <I18nextProvider i18n={i18n}>
+          <BrowserRouter>
+            <Provider store={store}>
+              <AuthProvider>
+                <SocketProvider socket={socket}>
+                  <App />
+                </SocketProvider>
+              </AuthProvider>
+            </Provider>
+          </BrowserRouter>
+        </I18nextProvider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
