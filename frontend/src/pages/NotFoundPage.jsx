8a0977820c23e36f2114e9fa9654f2855092c064
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-09-05 15:21:45 +0500

    fix: refactor `NotFoundPage`

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
-const NotFoundPage = () => <h1>Not found Page</h1>;
+import { ReactComponent as NotFound } from '../assets/404.svg';
+import routes from '../routes';
+
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="text-center">
+      <NotFound className="img-fluid h-25" alt={t('pages.notFound.imgAlt')} />
+      <h1 className="h4 text-muted">{t('pages.notFound.header')}</h1>
+      <p className="text-muted">
+        {t('pages.notFound.text')}
+        {' '}
+        <Link to={routes.homePage()}>{t('pages.notFound.link')}</Link>
+      </p>
+    </div>
+  );
+};
 
 export default NotFoundPage;
